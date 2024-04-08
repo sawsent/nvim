8b1d677d5a73e2f60833005eb07e029a4314e512
Vicente Figueiredo <vicenteppfigueiredo@gmail.com>
2024-04-08 20:51:41 +0100

    added lsp-config :DDDD

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -5,4 +5,12 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
+-- lsp keybinds 
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
+-- telescope: see telescope.lua
+
+-- neo-tree
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {} )
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,10 +3,7 @@ return {
     branch = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",       
+        "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
     },
-    config = function() 
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {} )
-    end
 }
