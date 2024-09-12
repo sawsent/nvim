04e902227ca093e1c11ed0454a95692b24d188c8
Vicente Figueiredo <sawsent@github.com>
2024-09-12 18:15:32 +0100

    removed problematic LSP servers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,9 +12,6 @@ return {
                 ensure_installed = {
                     'lua_ls',
                     'quick_lint_js',
-                    'cssls',
-                    'tsserver',
-                    'html',
                 }
             })
         end
@@ -25,9 +22,6 @@ return {
             local lspconfig = require('lspconfig')
             lspconfig.lua_ls.setup({})
             lspconfig.quick_lint_js.setup({})
-            lspconfig.cssls.setup({})
-            lspconfig.tsserver.setup({})
-            lspconfig.html.setup({})
         end
     }
 }
